dff9e3c3b76b0d9938ab6177d7779f4b6cf4e911
Sobhan Ghosh <sobhanghosh.jsr@gmail.com>
2025-06-11 12:44:55 +0530

    chore; update dependencies

--- WTpdF/wsgi.py
+++ WTpdF/wsgi.py
@@ -13,4 +13,8 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WTpdF.settings')
 
-application = get_wsgi_application()
+try:
+    application = get_wsgi_application()
+except Exception as e:
+    print(f"Error loading WSGI application: {e}")
+    raise
