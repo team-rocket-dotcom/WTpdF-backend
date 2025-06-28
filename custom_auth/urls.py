c2d4221b3661f06f65524b52485c10d3530d61c6
Sobhan Ghosh <sobhanghosh.jsr@gmail.com>
2025-06-28 16:25:05 +0530

    feat(urls): Add URL patterns for registration and Google OAuth views

--- custom_auth/urls.py
+++ custom_auth/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import LoginView
+from .views import LoginView, RegisterView, GoogleOAuthView
 
 app_name = 'custom_auth'
 
 urlpatterns = [
-    path('login', LoginView.as_view(), name='login')
+    path('register', RegisterView.as_view(), name='register'),
+    path('login', LoginView.as_view(), name='login'),
+    path('google', GoogleOAuthView.as_view(), name='google-oauth')
 ]
